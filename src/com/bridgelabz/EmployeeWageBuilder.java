b7c3e48b700a7dc66570bb07d3d27232f4f518d1
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-06 22:24:09 +0530

    Added: Part Time Employee

--- src/com/bridgelabz/EmployeeWageBuilder.java
+++ src/com/bridgelabz/EmployeeWageBuilder.java
@@ -5,16 +5,20 @@ import java.util.Random;
 public class EmployeeWageBuilder {
 
     public static void main(String[] args) {
-        int IS_FULL_TIME = 1;
+        int IS_FULL_TIME = 2;
+        int IS_PART_TIME = 1;
         int EMP_RATE_PER_HOUR = 20;
         int empHours = 0;
         int empWage = 0;
         // Taking Random values 0 or 1.
         Random random = new Random();
-        int empCheck = random.nextInt(2);
+        int empCheck = random.nextInt(3);
         // Checking the condition.
         if (empCheck == IS_FULL_TIME){
             empHours = 8;
+        }
+        else if (empCheck == IS_PART_TIME){
+            empHours = 4;
         }
         else {
             empHours = 0;
